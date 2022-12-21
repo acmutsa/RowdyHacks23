9eacf5db91daf324f380211fe8b433f9a5022adf
Liam Murray <liam@murray.to>
2022-12-21 14:07:36 -0600

    Changes + Bump Tailwind Version

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,8 @@ interface propsType {
 }
 
 const Home: NextPage<propsType> = ({ props }) => {
+	const router = useRouter();
+
 	return (
 		<div className="w-full h-screen absolute top-0 left-0">
 			<Parallax pages={3} className={'w-full overflow-hidden top-0 left-0'}>
@@ -79,7 +81,7 @@ const Home: NextPage<propsType> = ({ props }) => {
 						></img>
 					</div>
 				</ParallaxLayer>
-				<ParallaxLayer offset={1} speed={0} className="bg-[#2d112b]">
+				<ParallaxLayer offset={1} speed={0} className="cave-gradient">
 					<div className="w-full h-full grid grid-cols-2 max-w-[1000px] mx-auto pt-[100px]">
 						<div className="max-h-[280px] flex flex-col justify-center">
 							<h3 className="font-black font-permanent-marker italic text-white text-4xl mb-[10px]">
@@ -95,10 +97,33 @@ const Home: NextPage<propsType> = ({ props }) => {
 						<div className="flex items-center justify-center max-h-[280px]">
 							<Image src={'/img/logos/rh_landing.svg'} width={175} height={175}></Image>
 						</div>
-						<div></div>
+						<div className="col-span-2 grid grid-cols-3">
+							{/* {stats.map((stat, index) => (
+								<div
+									key={stat.data}
+									className={`${
+										index % 2 === 0 ? 'lg:ml-40 md:ml-20 ml-14' : 'md:mr-8 mr-24'
+									} text-center md:my-6 my-4`}
+								>
+									<p className="font-bold text-2xl text-indigo-600 lg:text-5xl">{stat.data}</p>
+									<p className="font-medium text-lg lg:text-3xl">{stat.object}</p>
+								</div>
+							))} */}
+							{/* {buttonDatas.map((button) => (
+								<div className="max-h-[4rem] flex items-center justify-center">
+									<button
+										key={button.text}
+										onClick={() => router.push(button.path)}
+										className="max-w-[12rem] w-[12rem] max-h-[4rem] md:max-w-full bg-indigo-300 py-4"
+									>
+										{button.text}
+									</button>
+								</div>
+							))} */}
+						</div>
 					</div>
 				</ParallaxLayer>
-				<ParallaxLayer offset={2} speed={0} className="z-10 bg-blue-500"></ParallaxLayer>
+				<ParallaxLayer offset={2} speed={0} className="z-10 bg-emerald-900"></ParallaxLayer>
 			</Parallax>
 		</div>
 	);
