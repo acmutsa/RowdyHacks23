e9d2dafd3c907ab7315155dddb8e26657ba65ba4
Liam Murray <liam@murray.to>
2022-12-23 15:06:21 -0600

    Update Profile Menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ const Home: NextPage<propsType> = ({ props }) => {
 				<ParallaxLayer offset={1} speed={0} className="bg-[#2d112b]">
 					<div className="w-full h-full grid grid-cols-2 max-w-[1000px] mx-auto pt-[100px]">
 						<div className="max-h-[280px] flex flex-col justify-center">
-							<h3 className="font-black font-permanent-marker italic text-white text-4xl mb-[10px]">
+							<h3 className="font-black font-permanent-marker text-white text-4xl mb-[10px]">
 								About Us
 							</h3>
 							<p className="font-sans text-white text-lg font-bold">
