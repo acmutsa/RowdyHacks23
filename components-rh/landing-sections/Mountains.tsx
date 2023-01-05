d86da8659325d36def9c0b77a1d96ea3f14e9c0c
Liam Murray <liam@murray.to>
2023-01-05 12:50:33 -0600

    Begin Mobile Optimizations

--- components-rh/landing-sections/Mountains.tsx
+++ components-rh/landing-sections/Mountains.tsx
@@ -37,7 +37,7 @@ export const Mountains: FunctionComponent = () => {
 			</motion.div>
 			<motion.div
 				style={{ y: header }}
-				className="w-full flex justify-center absolute items-center pt-[calc(100vh/4)] will-change-transform"
+				className="w-full flex md:flex-row flex-col justify-center absolute items-center pt-[calc(100vh/4)] will-change-transform"
 			>
 				<Image
 					src="/img/logos/rh_landing.svg"
@@ -46,8 +46,8 @@ export const Mountains: FunctionComponent = () => {
 					className="h-[100px] w-[100px]"
 				></Image>
 				<div className="ml-[5px]">
-					<h1 className="text-8xl font-black font-sans text-white">RowdyHacks</h1>
-					<h2 className="text-4xl font-black font-permanent-marker italic text-white text-center">
+					<h1 className="md:text-8xl text-5xl font-black font-sans text-white">RowdyHacks</h1>
+					<h2 className="md:text-4xl text-3xl font-black font-permanent-marker italic text-white text-center">
 						Into The Unknown
 					</h2>
 				</div>
@@ -101,12 +101,12 @@ export const Mountains: FunctionComponent = () => {
 export const MountainCaves: FunctionComponent = () => {
 	return (
 		<section className="min-h-screen bg-[#2d112b] relative z-10">
-			<div className="w-full min-h-screen grid grid-cols-2 grid-rows-2 max-w-[1000px] mx-auto pt-[100px]">
-				<div className="max-h-[280px] flex flex-col justify-center">
-					<h3 className="font-black font-permanent-marker text-white text-4xl mb-[10px]">
+			<div className="w-full min-h-screen grid md:grid-cols-2 grid-cols-1 max-w-[1000px] mx-auto pt-[100px]">
+				<div className="max-h-[280px] flex flex-col justify-center px-[5px]">
+					<h3 className="font-black font-permanent-marker text-white text-4xl mb-[10px] text-center md:text-left">
 						About Us
 					</h3>
-					<p className="font-sans text-white text-lg font-bold">
+					<p className="font-sans text-white text-lg font-bold text-center md:text-left">
 						RowdyHacks is a free, weekend-long, overnight hackathon hosted at UTSA! Students can
 						join us to network, code, collaborate, and compete. We welcome hackers from all
 						disciplines, backgrounds, & technical levels!
@@ -122,14 +122,14 @@ export const MountainCaves: FunctionComponent = () => {
 						<h1 className="w-full font-permanent-marker tracking-wider">ROWDY</h1>
 					</div>
 				</div>
-				<div className="flex items-start justify-center">
+				<div className="md:flex items-start justify-center hidden">
 					<Image src={'/img/mountain_camping_site.png'} width={280} height={280} />
 				</div>
-				<div className="max-h-[280px] flex flex-col justify-center">
-					<h3 className="font-black font-permanent-marker text-white text-4xl mb-[10px]">
+				<div className="max-h-[280px] flex flex-col justify-center px-[5px]">
+					<h3 className="font-black font-permanent-marker text-white text-4xl mb-[10px] md:mt-0 mt-[100px] text-center md:text-left">
 						Why attend?
 					</h3>
-					<p className="font-sans text-white text-lg font-bold">
+					<p className="font-sans text-white text-lg font-bold text-center md:text-left">
 						RowdyHacks is a free, weekend-long, overnight hackathon hosted at UTSA and the second
 						largest hackathon in Texas! Students can join us to network, code, collaborate, and
 						compete. We welcome hackers from all disciplines, backgrounds, & technical levels!
