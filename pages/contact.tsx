3572b96b8f691a8180af6f2a3984fc46fa291e72
Liam Murray <liam@murray.to>
2023-01-25 12:56:02 -0600

    Update Contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,7 +5,7 @@ const Contact: NextPage = () => {
 		<div className='absolute top-0 min-h-screen w-full bg-[url("https://static.rowdyhacks.org/img/profiles/mountainbg.svg")] bg-cover bg-fixed flex items-center justify-center'>
 			<div className="w-full max-w-[1000px] md:aspect-video md:h-auto h-[500px] p-5 rounded border-2 border-rh-sunset bg-rh-deep-purple flex flex-col items-center justify-center">
 				<h1 className="text-6xl font-poppins font-black text-rh-sunset">Contact Us</h1>
-				<h2 className="font-sans text-white mt-3 font-bold">
+				<h2 className="font-sans text-white text-center mt-3 font-bold">
 					Have a question and want to reach out? Feel free to use one of the contact methods below!
 				</h2>
 			</div>
