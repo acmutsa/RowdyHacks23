e8211a6dfeebebc419f50b404e26c8244b7c0997
joshuasilva414 <joshuasilva414@gmail.com>
2023-03-24 19:21:54 -0500

    Dashboard rocks now

--- pages/dashboard/Components/SpotlightCard.tsx
+++ pages/dashboard/Components/SpotlightCard.tsx
@@ -12,76 +12,77 @@ import firebase from 'firebase';
  */
 
 function SpotlightCard(props: any) {
-  const startDate = new firebase.firestore.Timestamp(props.startDate._seconds, 0).toDate();
-  const endDate = new firebase.firestore.Timestamp(props.endDate._seconds, 0).toDate();
+	const startDate = new firebase.firestore.Timestamp(props.startDate._seconds, 0).toDate();
+	const endDate = new firebase.firestore.Timestamp(props.endDate._seconds, 0).toDate();
 
-  const speakersData = props.speakers
-    ? props.speakers.filter((speaker: string) => speaker.length !== 0)
-    : undefined;
+	const speakersData = props.speakers
+		? props.speakers.filter((speaker: string) => speaker.length !== 0)
+		: undefined;
 
-  var speakerString = '';
-  if (speakersData !== undefined && speakersData !== null && speakersData.length !== 0) {
-    if (speakersData.length == 2) {
-      speakerString = `Hosted by ${speakersData[0]} & ${speakersData[1]}`;
-    } else if (speakersData.length == 1) {
-      speakerString = `Hosted by ${speakersData[0]}`;
-    } else {
-      speakerString = 'Hosted by ';
-      for (var i = 0; i < speakersData.length; i++) {
-        if (i === speakersData.length - 1) {
-          speakerString += 'and ' + speakersData[i];
-        } else {
-          speakerString += speakersData[i] + ', ';
-        }
-      }
-    }
-  }
+	var speakerString = '';
+	if (speakersData !== undefined && speakersData !== null && speakersData.length !== 0) {
+		if (speakersData.length == 2) {
+			speakerString = `Hosted by ${speakersData[0]} & ${speakersData[1]}`;
+		} else if (speakersData.length == 1) {
+			speakerString = `Hosted by ${speakersData[0]}`;
+		} else {
+			speakerString = 'Hosted by ';
+			for (var i = 0; i < speakersData.length; i++) {
+				if (i === speakersData.length - 1) {
+					speakerString += 'and ' + speakersData[i];
+				} else {
+					speakerString += speakersData[i] + ', ';
+				}
+			}
+		}
+	}
+	// console.log('Speaker String: ', speakerString);
 
-  //first match extracts day abbreviation
-  //second match extracts month abbreviation and the number day of the month
-  var dayString =
-    startDate.toString().match(/^[\w]{3}/)[0] +
-    ', ' +
-    startDate.toString().match(/^\w+ (\w{3} \d{1,2})/)[1];
+	//first match extracts day abbreviation
+	//second match extracts month abbreviation and the number day of the month
+	var dayString =
+		startDate.toString().match(/^[\w]{3}/)[0] +
+		', ' +
+		startDate.toString().match(/^\w+ (\w{3} \d{1,2})/)[1];
 
-  return (
-    <>
-      <div className="scrollItem flex flex-col justify-between min-w-3/4 h-[90%] bg-aqua rounded-lg p-3 my-4 mx-12">
-        <h1 className="lg:text-4xl text-xl font-black">{props.title}</h1>
-        <h3 className="md:text-md text-sm font-black">{speakerString}</h3>
-        {/* info section */}
-        <div className="flex flex-col justify-around w-full min-h-1/2 rounded-lg bg-darkAqua p-3">
-          {/* top row info */}
-          <div className="flex justify-around">
-            <div className="flex items-center lg:text-lg sm:text-md text-xs">
-              {<CalendarIcon style={{ fontSize: 'medium', margin: '2px' }} />}
-              <p>{dayString}</p>
-            </div>
-            <div className="flex items-center lg:text-lg sm:text-md text-xs">
-              {<PinDrop style={{ fontSize: 'medium', margin: '2px' }} />}
-              <p>{props.location}</p>
-            </div>
-          </div>
-          {/* botton row info */}
-          <div className="flex justify-around">
-            <div className="flex items-center lg:text-lg sm:text-md text-xs">
-              {<ClockIcon style={{ fontSize: 'large', margin: '2px' }} />}
-              <p>
-                {(startDate.getHours() + 24) % 12 || 12}:{startDate.getMinutes() < 10 ? '0' : ''}
-                {startDate.getMinutes()} {startDate.getHours() < 12 ? 'AM' : 'PM'} -{' '}
-                {(endDate.getHours() + 24) % 12 || 12}:{endDate.getMinutes() < 10 ? '0' : ''}
-                {endDate.getMinutes()} {endDate.getHours() < 12 ? 'AM' : 'PM'}
-              </p>
-            </div>
-            <div className="flex items-center lg:text-lg sm:text-md text-xs">
-              {<Backpack style={{ fontSize: 'medium', margin: '2px' }} />}
-              <p>{props.page}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="scrollItem flex flex-col justify-between min-w-3/4 h-[90%] bg-aqua rounded-lg p-3 px-0 my-4 mx-12">
+				<h1 className="lg:text-2xl text-4xl md:text-2xl font-black">{props.title}</h1>
+				<h3 className="md:text-md text-sm font-black">{speakerString}</h3>
+				{/* info section */}
+				<div className="flex flex-col justify-around w-full min-h-1/2 rounded-lg bg-darkAqua p-3 pl-0 max-w-sm">
+					{/* top row info */}
+					<div className="flex sm:flex-row flex-col justify-between">
+						<div className="flex gap-1 items-center text-xl">
+							{<CalendarIcon style={{ fontSize: 'medium', margin: '2px' }} />}
+							<p>{dayString}</p>
+						</div>
+						<div className="flex gap-1 items-center text-xl">
+							{<PinDrop style={{ fontSize: 'medium', margin: '2px' }} />}
+							<p>{props.location}</p>
+						</div>
+					</div>
+					{/* botton row info */}
+					<div className="flex sm:flex-row flex-col justify-between">
+						<div className="flex items-center text-xl">
+							{<ClockIcon style={{ fontSize: 'large', margin: '2px' }} />}
+							<p>
+								{(startDate.getHours() + 24) % 12 || 12}:{startDate.getMinutes() < 10 ? '0' : ''}
+								{startDate.getMinutes()} {startDate.getHours() < 12 ? 'AM' : 'PM'} -{' '}
+								{(endDate.getHours() + 24) % 12 || 12}:{endDate.getMinutes() < 10 ? '0' : ''}
+								{endDate.getMinutes()} {endDate.getHours() < 12 ? 'AM' : 'PM'}
+							</p>
+						</div>
+						<div className="flex items-center text-xl">
+							{<Backpack style={{ fontSize: 'medium', margin: '2px' }} />}
+							<p>{props.page}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default SpotlightCard;
